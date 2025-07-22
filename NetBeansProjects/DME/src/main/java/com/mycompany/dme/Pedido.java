fc9881303b6b145400c9558f1e3655fcd33ba1da
Clara Luisa <claraluisa2@gmail.com>
2025-07-22 17:30:18 -0300

    first commit

--- NetBeansProjects/DME/src/main/java/com/mycompany/dme/Pedido.java
+++ NetBeansProjects/DME/src/main/java/com/mycompany/dme/Pedido.java
@@ -13,4 +13,45 @@ public class Pedido {
     private String data;
     private String formaPagamento;
     private int quantProdutos;
+
+    public Pedido(int numPedido, String data, String formaPagamento, int quantProdutos) {
+        this.numPedido = numPedido;
+        this.data = data;
+        this.formaPagamento = formaPagamento;
+        this.quantProdutos = quantProdutos;
+    }
+
+    public int getNumPedido() {
+        return numPedido;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public int getQuantProdutos() {
+        return quantProdutos;
+    }
+
+    public void setNumPedido(int numPedido) {
+        this.numPedido = numPedido;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public void setQuantProdutos(int quantProdutos) {
+        this.quantProdutos = quantProdutos;
+    }
+    
+    
 }
